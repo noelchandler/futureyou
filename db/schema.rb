dfaf04bbe5de754d686c1b42bcf590c6cbbf640d
noelchandler <noel@mosio.com>
2014-06-16 13:27:09 -0600

    Created Children Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605175006) do
+ActiveRecord::Schema.define(version: 20140616191934) do
+
+  create_table "children", force: true do |t|
+    t.string   "name"
+    t.date     "birthday"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
